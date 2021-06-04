f4e145905d49f91d78254b948a1f631b60351d28
wenbo.dong <wenbo.dong@appshahe.com>
2021-06-04 14:39:14 +0800

    go-log 第一次提交

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-log
+
+go 1.15
+
+require github.com/Shopify/sarama v1.29.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package go_log
+
+import "github.com/Shopify/sarama"
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll          //发送完数据，需要leader和follow都确认
+	config.Producer.Partitioner = sarama.NewRandomPartitioner //新选出一个partition
+	config.Producer.Return.Successes = true                   //成功交付的消息将在success返回
+}
