a4e1523f43123d0a56c4cf34342291a2d4d43c36
wenbo.dong <wenbo.dong@appshahe.com>
2021-06-04 15:32:56 +0800

    go-log 第2次提交

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package go_log
 
-import "github.com/Shopify/sarama"
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
 
 func main() {
+	fmt.Println(111)
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll          //发送完数据，需要leader和follow都确认
 	config.Producer.Partitioner = sarama.NewRandomPartitioner //新选出一个partition
